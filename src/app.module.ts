15d1440ff0b95429e90f446c9ac89824871281ad
Manuel Szecsenyi <manuel.szecsenyi@gmail.com>
2022-05-17 18:25:57 +0200

    first image rendered

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PosterController } from './poster/poster/poster.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, PosterController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/poster/poster/poster.controller.ts
@@ -0,0 +1,41 @@
+import { Controller, Get } from '@nestjs/common';
+import { createCanvas, loadImage } from 'canvas';
+
+@Controller('poster')
+export class PosterController {
+
+    constructor(
+
+    ){}
+
+    @Get()
+    getPoster(): any {
+
+        const canvas = createCanvas(200, 200)
+
+        const ctx = canvas.getContext('2d')
+        // Write "Awesome!"
+        ctx.font = '30px Impact'
+        ctx.rotate(0.1)
+        ctx.fillText('Awesome!', 50, 100)
+
+        // Draw line under text
+        var text = ctx.measureText('Awesome!')
+        ctx.strokeStyle = 'rgba(0,0,0,0.5)'
+        ctx.beginPath()
+        ctx.lineTo(50, 102)
+        ctx.lineTo(50 + text.width, 102)
+        ctx.stroke()
+
+        // Draw cat with lime helmet
+        loadImage('examples/images/lime-cat.jpg').then((image) => {
+        ctx.drawImage(image, 50, 0, 70, 70)
+
+        console.log('<img src="' + canvas.toDataURL() + '" />')
+        })
+
+        return '<img src="' + canvas.toDataURL() + '" />';
+
+    }
+
+}
