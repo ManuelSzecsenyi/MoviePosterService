a352a50bceb22094f63fce2105023078cbfb7d09
Manuel Szecsenyi <manuel.szecsenyi@gmail.com>
2022-06-12 10:07:23 +0200

    Added iconset to movie poster

--- src/poster/poster/poster.controller.ts
+++ src/poster/poster/poster.controller.ts
@@ -16,12 +16,12 @@ export class PosterController {
         return this.getPoster({
             posterUrl: "https://image.posterlounge.at/images/m/1913709.jpg",
             name: "wwwwwwwwwwwwwwwwwwwwwwww",
-            availableAt: ["netflix"]
+            availableAt: ["netflix", "prime"]
         })
     }
 
     @Post()
-    getPoster(@Body() movie): any {
+    getPoster(@Body() movie: IMovie): any {
 
         const posterUrl = movie.posterUrl; // "https://image.posterlounge.at/images/m/1913709.jpg";
         let title = movie.name.slice(0, 18); //"Testitle for movie"
@@ -41,8 +41,10 @@ export class PosterController {
 
         return zip(
             loadImage(posterUrl),
-            //loadImage("./disney-plus_logo.png")
-        ).toPromise().then( ([image] ) => {
+            loadImage("https://marginleft.at/fh/disney-plus_logo.png"),
+            loadImage("https://marginleft.at/fh/netflix_logo.png"),
+            loadImage("https://marginleft.at/fh/prime_logo.png")
+        ).toPromise().then( ([image, disney, netflix, prime] ) => {
 
             ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 
@@ -66,69 +68,36 @@ export class PosterController {
 
             console.log(movie)
 
-            if(movie.availableAt == "disney") {
-                ctx.fillText("Disney", 180, canvas.height - canvas.height / 15.5)
+
+            if(movie.availableAt.includes("disney")) {
+                ctx.globalAlpha = 1
+                ctx.drawImage(disney, 160, 595, 50, 50)
+            } else {
+                ctx.globalAlpha = 0.3
+                ctx.drawImage(disney, 160, 595, 50, 50)
             }
 
-            if(movie.availableAt == "netflix") {
-                ctx.fillText("Neflix", 260, canvas.height - canvas.height / 15.5)
+            if(movie.availableAt.includes("netflix")) {
+                ctx.globalAlpha = 1
+                ctx.drawImage(netflix, 220, 595, 50, 50)
+            } else {
+                ctx.globalAlpha = 0.3
+                ctx.drawImage(netflix, 220, 595, 50, 50)
             }
 
-            if(movie.availableAt == "prime") {
-                ctx.fillText("Prime", 320, canvas.height - canvas.height / 15.5)
-            }
 
+            if(movie.availableAt.includes( "prime")) {
+                ctx.globalAlpha = 1
+                ctx.drawImage(prime, 280, 595, 50, 50)
+            } else {
+                ctx.globalAlpha = 0.3
+                ctx.drawImage(prime, 280, 595, 50, 50)
+            }
 
             //return canvas.toDataURL();
-        
             return '<img src="' + canvas.toDataURL() + '" />'; 
         })
 
-
-        return; 
-        // Poster background
-        return loadImage(posterUrl).then((image) => {
-            
-            ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-
-            const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
-            
-            gradient.addColorStop(0, 'rgba(0, 0, 0, 0.0)');
-            gradient.addColorStop(1, 'black');
-
-            ctx.fillStyle = gradient;
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-
-            ctx.font = "37px Arial";
-            ctx.fillStyle = "white";
-            ctx.fillText(title, 20, canvas.height - canvas.height / 7.5)
-
-            ctx.font = "24px Arial";
-            ctx.fillText("Available at", 20, canvas.height - canvas.height / 15.5)
-
-            ctx.font = "12px Arial";
-
-            console.log(movie)
-
-            if(movie.availableAt == "disney") {
-                ctx.fillText("Disney", 180, canvas.height - canvas.height / 15.5)
-            }
-
-            if(movie.availableAt == "netflix") {
-                ctx.fillText("Neflix", 260, canvas.height - canvas.height / 15.5)
-            }
-
-            if(movie.availableAt == "prime") {
-                ctx.fillText("Prime", 320, canvas.height - canvas.height / 15.5)
-            }
-
-
-            //return canvas.toDataURL();
-        
-            return '<img src="' + canvas.toDataURL() + '" />'; 
-            
-        });        
 
     }
 
